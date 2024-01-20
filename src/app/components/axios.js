afd1a9f402a423f1b3029f4d38ab21754deb9186
Faklor <faklor_br_ar@mail.ru>
2024-01-20 13:19:57 +0300

    procces

--- src/app/components/axios.js
+++ src/app/components/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const getDataNow = async(tag)=>await axios.get('https://api.nasa.gov/planetary/apod?api_key=qhEGoh68nycYipATXrh7qWgTSv4PZEPQbx5sdL26&'+tag)
+const getDataNow = async(tag,progress)=>await axios.get('https://api.nasa.gov/planetary/apod?api_key=qhEGoh68nycYipATXrh7qWgTSv4PZEPQbx5sdL26&'+tag,{onDownloadProgress:progress})
 
 
 
--- src/app/components/slyder.tsx
+++ src/app/components/slyder.tsx
@@ -7,18 +7,32 @@ import {
 import {
     slyder
 } from './animate'
+// import nProgress from 'nprogress'
+// import Router from 'next/router'
+// import 'nprogress/nprogress.css'
+// //import {useRouter} from 'next/navigation'
+// Router.events.on('routeChangeStart', () => nProgress.start())
+// Router.events.on('routeChangeComplete', () => nProgress.done());
+// Router.events.on('routeChangeError', () => nProgress.done())
 
 
 export default function Slyder({date,validDay,validPeriod}:{date:string, validDay:boolean, validPeriod:boolean}){
     //hook
     const [images, setImages] = useState<Array<string>>([])
     const [error, setError] = useState<boolean>(false)
-
+    const [load, setLoad] = useState<string>('loading...')
+    
+    function progress(event:any){
+        if(event.loaded){
+            setLoad('')
+            
+        }
+    }
     
     //load
     useEffect(()=>{
         if(validDay){
-            getDataNow('date='+date)
+            getDataNow('date='+date,progress)
             .then(res=>{
                  
                 //console.log(res.config.onDownloadProgress())
@@ -38,7 +52,7 @@ export default function Slyder({date,validDay,validPeriod}:{date:string, validDa
         else if(validPeriod){
             let days:string[] = date.split('/')
             
-            getDataNow(`start_date=${days[0]}&end_date=${days[1]}`)
+            getDataNow(`start_date=${days[0]}&end_date=${days[1]}`,progress)
             .then(res=>{
                 let array:string[] = []
     
@@ -60,7 +74,7 @@ export default function Slyder({date,validDay,validPeriod}:{date:string, validDa
         
         <div className="slyder">
             
-            {error?<h3>Incorrect data:check if the date or range is entered correctly</h3>:<></>}
+            {error?<h3>Incorrect data:check if the date or range is entered correctly</h3>:<h3>{load}</h3>}
             
             {images.map((img,index)=>{
                 return <Image src={img} key={index}  alt={``} width={500} height={500}/>
